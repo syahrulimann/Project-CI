ea43f28c91b44fc5f2a572ca839f9abb7d0c881a
syahrulimann <128056296+syahrulimann@users.noreply.github.com>
2023-06-13 07:43:37 +0700

    Delete application directory

--- application/helpers/rupiah_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-function rupiah($angka)
-{
-    $hasil = 'Rp. ' . number_format($angka, 0, ",", ".");
-    return $hasil;
-}
